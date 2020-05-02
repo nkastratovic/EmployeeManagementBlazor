8a17e6925078e7087ee34def2cac886cd7a2a9da
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2020-05-02 08:14:12 +0200

    Component parameters

---
+++ EmployeeManagement.Web/Pages/DataBindingDemoBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DataBindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "Tom";
+        protected string Gender { get; set; } = "Male";
+        protected string Colour { get; set; } = "background-color:white";
+        protected string Description { get; set; } = string.Empty;
+    }
+}
--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -14,6 +14,8 @@ namespace EmployeeManagement.Web.Pages
 
         public IEnumerable<Employee> Employees { get; set; }
 
+        public bool ShowFooter { get; set; } = true;
+
         protected override async Task OnInitializedAsync()
         {
             Employees = (await EmployeeService.GetEmployees()).ToList();
